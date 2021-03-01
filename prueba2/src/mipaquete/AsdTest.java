bca63c06312cbbd4a42f12e7283501c43a18fd26
Zther <Zther@Zther-Dsk>
2021-03-01 09:08:09 +0100

    primer cambio a la rama maestra

--- prueba2/src/mipaquete/AsdTest.java
+++ prueba2/src/mipaquete/AsdTest.java
@@ -3,6 +3,7 @@ package mipaquete;
 public class AsdTest {
 	public static void main (String[]) {
 		System.out.println("esta es la rama MASTER");
+		System.out.println("modificamos la rama maestra, a ver que pasa");
 	}
 
 }
