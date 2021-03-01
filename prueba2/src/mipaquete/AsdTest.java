0313a8661f2f0537a373ef6dcc8f015dca4defc5
Zther <Zther@Zther-Dsk>
2021-03-01 09:05:46 +0100

    creamos la rama_1

--- prueba2/src/mipaquete/AsdTest.java
+++ prueba2/src/mipaquete/AsdTest.java
@@ -3,6 +3,7 @@ package mipaquete;
 public class AsdTest {
 	public static void main (String[]) {
 		System.out.println("esta es la rama MASTER");
+		System.out.println("estamos creando y tabajando en la Rama_1");
 	}
 
 }
