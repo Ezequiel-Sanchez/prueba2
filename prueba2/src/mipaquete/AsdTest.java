1bb4b5abae1e23fb61034d8ec0dd57c19f6bf0ef
Zther <Zther@Zther-Dsk>
2021-03-08 08:55:43 +0100

    modificamos la rama 1

--- prueba2/src/mipaquete/AsdTest.java
+++ prueba2/src/mipaquete/AsdTest.java
@@ -1,9 +1,10 @@
 package mipaquete;
 
 public class AsdTest {
-	public static void main (String[]) {
+	public static void main (String arg[]) {
 		System.out.println("esta es la rama MASTER");
 		System.out.println("estamos creando y tabajando en la Rama_1");
+		System.out.println("estamos tabajando en la Rama_1");
 	}
 
 }
