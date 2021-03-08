7ce251c29204f06e571bfd239406c54a106712e2
Zther <Zther@Zther-Dsk>
2021-03-08 08:47:36 +0100

    faltaba el "arg" en el void main

--- prueba2/src/mipaquete/AsdTest.java
+++ prueba2/src/mipaquete/AsdTest.java
@@ -1,7 +1,7 @@
 package mipaquete;
 
 public class AsdTest {
-	public static void main (String[]) {
+	public static void main (String arg[]) {
 		System.out.println("esta es la rama MASTER");
 		System.out.println("modificamos la rama maestra, a ver que pasa");
 	}
